84e3052e7a65d0687edc9b2eb8d77508fc08cdc5
Miriam Peralta <miiriampc@gmail.com>
2017-07-20 18:03:05 -0500

    section validate in construction

--- src/assets/js/componentes/validate.js
+++ src/assets/js/componentes/validate.js
@@ -1,31 +1,29 @@
 const ValidateRuc = (update) => {
     const formContainer = $('<form action=""></form>');
+    const div = $('<div class=""></div>');
     const labelRuc = $('<label for="ruc">RUC</label>');
-    const inputRuc = $('<input id="ruc" type="number">');
+    const inputRuc = $('<input id="ruc" type="number" placeholder="Ingresa RUC de 12 dígitos">');
     const btnValidate = $('<input id="consultar" type="submit" value="Consultar">');
+    const message = $('<span></span>');
 
     formContainer.append(labelRuc);
     formContainer.append(inputRuc);
     formContainer.append(btnValidate);
 
-    inputRuc.on('keydown keyup',(e) =>{
-
-    });
-
     btnValidate.on('click', (e) =>{
         e.preventDefault();
-        const ruc = inputRuc.val();
         const tecactusApi = new TecactusApi("dTBvBWAG9zNqDaIdYUPaPxirTypgikwWEvUVDJqT");
-        tecactusApi.Sunat.getByRuc(ruc)
+        tecactusApi.Sunat.getByRuc(inputRuc.val())
             .then(function (response) {
-                console.log("consulta correcta!")
-                console.log(response.data)
+                message.empty();
+                console.log("consulta correcta!");
+                message.text(response.data);
             })
             .catch(function (response) {
-                console.log("algo ocurrió")
-                console.log("código de error: " + response.code)
-                console.log("mensaje de respuesta: " + response.status)
-                console.log(response.data)
+                console.log("algo ocurrió");
+                console.log("código de error: " + response.code);
+                console.log("mensaje de respuesta: " + response.status);
+                console.log(response.data);
             })
     });
     return formContainer;
